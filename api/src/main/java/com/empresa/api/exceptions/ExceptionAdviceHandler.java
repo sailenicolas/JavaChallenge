4da8c574b8af13f5c83c6b7652af3276f2feae0f
Elias Nicolas Gomez <elias.gomez@security.cl>
2026-02-02 10:39:13 -0300

    Added Error Handling

--- api/src/main/java/com/empresa/api/exceptions/ExceptionAdviceHandler.java
+++ api/src/main/java/com/empresa/api/exceptions/ExceptionAdviceHandler.java
@@ -1,8 +1,25 @@
 package com.empresa.api.exceptions;
 
+import com.empresa.core.exceptions.ApiError;
 import com.empresa.core.exceptions.ExceptionMVCHandler;
+import jakarta.validation.ConstraintViolationException;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionAdviceHandler extends ExceptionMVCHandler {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    protected ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex) {
+        Set<String> collect = ex.getConstraintViolations().parallelStream()
+                .map(o -> "%s: %s".formatted(o.getPropertyPath().toString().split("\\.")[1], o.getMessage())).collect(Collectors.toSet());
+        ApiError body = new ApiError();
+        body.setMessage("Validation Error Call");
+        body.setErrors(collect.parallelStream().toList());
+        return ResponseEntity.status(400).body(body);
+    }
+
 }
